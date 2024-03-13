936d26b003af64f1e999dc87802b024b9686e6e1
Nikhil Dubey <officialnikhildubey@gmail.com>
2024-03-13 21:52:27 +0530

    day1 intro to programming and python

--- day1.py
+++ day1.py
@@ -1,1 +1,22 @@
-print("HEllo")
+'''
+Programming is the way for us to tell computers what to do.
+Python Programming Language
+Guido van rossum
+Dynamically Typed 
+Object Oriented Pardigm
+1989
+Simple Programming Language
+Platform Independent
+Open Source Programming Language
+It provides very big library support
+It is possible to integrate  other languages into Python
+Can be used in Web Development, Data Analysis
+Machine Learning AI Databases
+REPL - Read Evaluate PRint Loop
+high level programming language
+REPLIt - Online code editor
+'''
+
+
+print("Hello World")
+print(5)
