a099408d32683fabeaf5f36b2c2fe49db41c9703
Nikhil Dubey <nikhil.dubey@avantika.edu.in>
2024-04-22 14:41:03 +0530

    day12

--- day11.py
+++ day11.py
@@ -1,1 +1,18 @@
 #Strings
+name = "Nikhil"
+friend = "Yash"
+
+print("Hello, " + name)
+
+apple = '''He said,
+I want 
+to eat an
+apple.'''
+print(apple)
+
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
---
+++ day12.py
@@ -0,0 +1,6 @@
+#Strings Slicing and Operations
+
+names = "Harry,Shubham"
+print(names[0:5])
+print(len(names))
+
